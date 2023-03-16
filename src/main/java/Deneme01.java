8024d28f4584a3c5c3fc696bec7c615cd2a7476c
esrakbc <122485941+esrakbc@users.noreply.github.com>
2023-03-17 01:21:27 +0300

    Revert "v2"

--- src/main/java/Deneme01.java
+++ src/main/java/Deneme01.java
@@ -2,16 +2,5 @@ public class Deneme01 {
     public static void main(String[] args) {
         System.out.println("version ");
         //github ta yazdıklarım
-
-
-
-
-
-
-
-
-
-     //oley basardik .....
-
     }
 }
